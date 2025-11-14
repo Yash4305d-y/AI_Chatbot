d35ff6c6d3c22c3222a23a52fc1345e419a63ced
Yash Vinay Kalyani <howbow4305@gmail.com>
2025-11-14 19:41:28 +0530

    Clean commit without secrets

---
+++ MAIN.py
@@ -0,0 +1,24 @@
+from groq import Groq
+
+client = Groq(api_key="API_KEY")
+
+def chat_with_bot(prompt):
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    
+    return response.choices[0].message.content.strip()
+
+if __name__ == "__main__":
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["quit", "exit", "bye"]:
+            break
+
+        reply = chat_with_bot(user_input)
+        print("Chatbot:", reply)
+        print("\n")
